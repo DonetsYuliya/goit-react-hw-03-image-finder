6a4025d3109fb332b9f820c86be4baa852d94801
DonetsYuliya <donecjuli@gmail.com>
2023-01-30 12:37:16 +0200

    creating hw-03 images finder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,77 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { getImages } from 'services/api';
+import { Searchbar } from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
+
+export class App extends Component {
+  state = {
+    search: '',
+    images: [],
+    page: 1,
+    loading: false,
+    currentImage: null,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { search, page } = this.state;
+    if (prevState.search !== search || prevState.page !== page) {
+      this.fetchImages();
+    }
+  }
+
+  async fetchImages() {
+    try {
+      this.setState({ loading: true });
+      const { search, page } = this.state;
+      const data = await getImages(search, page);
+      this.setState(({ images }) => ({
+        images: [...images, ...data],
+      }));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  getImages = ({ search }) => {
+    this.setState({ search, images: [], page: 1 });
+  };
+
+  loadMore = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
+
+  openModal = data => {
+    this.setState(prevState => {
+      return { currentImage: data };
+    });
+  };
+
+  closeModal = () => {
+    this.setState({ currentImage: null });
+  };
+
+  render() {
+    const { images, loading, currentImage } = this.state;
+    const { getImages, loadMore, openModal, closeModal } = this;
+    return (
+      <div>
+        <Searchbar onSubmit={getImages} />
+        <ImageGallery images={images} openModal={openModal} />
+        {loading && <Loader />}
+        {Boolean(images.length) && (
+          <Button text="Load more..." handleClick={loadMore} />
+        )}
+        {currentImage && (
+          <Modal currentImage={currentImage} closeModal={closeModal} />
+        )}
+      </div>
+    );
+  }
+}
